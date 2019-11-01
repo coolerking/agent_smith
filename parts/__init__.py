48be24e70367e62d6f607f55c93e035c1d19d265
Tasuku Hori <tasuku-hori@exa-corp.co.jp>
2019-11-01 13:28:35 +0900

    import元修正

--- parts/__init__.py
+++ parts/__init__.py
@@ -4,7 +4,7 @@ from .forklift_joystick import get_js_controller
 from .forklift_controller import LocalWebForkliftController
 from .pigpio_wrapper import PIGPIO_IN, PIGPIO_OUT, PIGPIO_PWM, PIGPIO_SPI_ADC
 from .sensors.range import get_range_part
-from .sensors.navigation import HedgehogController
+from .sensors.navigation import HedgehogController, FormerHedgehogPusher
 from .clock import Timestamp
 from .led_status import LED, RGB_LED
-from .image import MapImageCreator, FormerHedgehogPusher
+from .image import MapImageCreator
